2db886b0c32208ad3bafd431507ced6db51935be
Rut Patel <22752504+darkerror96@users.noreply.github.com>
2019-04-06 23:47:15 -0400

    Create UniCodeMagic.java

---
+++ BasicJava/src/trick/UniCodeMagic.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package trick;
+
+/**
+ * @author rutpatel
+ *
+ */
+public class UnicodeMagic {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// the line below this gives an output
+		// \u000d System.out.println("comment executed");
+
+	}
+
+}
