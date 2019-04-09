21a9adf31eb8f9133c5c36277e618720102e1ab2
Rut Patel <22752504+darkerror96@users.noreply.github.com>
2019-04-09 16:47:36 -0400

    Update UniCodeMagic.java

--- BasicJava/src/trick/UniCodeMagic.java
+++ BasicJava/src/trick/UniCodeMagic.java
@@ -13,7 +13,10 @@ public class UnicodeMagic {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		
+		// no error in naming variable as below
+		int $_ = 5;
+		
 		// the line below this gives an output
 		// \u000d System.out.println("comment executed");
 
