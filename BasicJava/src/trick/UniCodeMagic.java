7d8098258ded0f0b4f006048d732d90fcd910081
Rut Patel <22752504+darkerror96@users.noreply.github.com>
2019-04-09 16:49:04 -0400

    Update UniCodeMagic.java

--- BasicJava/src/trick/UniCodeMagic.java
+++ BasicJava/src/trick/UniCodeMagic.java
@@ -14,12 +14,16 @@ public class UnicodeMagic {
 	 */
 	public static void main(String[] args) {
 		
+		// the line below this gives an output
+		// \u000d System.out.println("comment executed");
+		
 		// no error in naming variable as below
 		int $_ = 5;
 		
-		// the line below this gives an output
-		// \u000d System.out.println("comment executed");
-
+		long longWithL = 1000*60*60*24*365L;
+		long longWithoutL = 1000*60*60*24*365;
+		System.out.println(longWithL);
+		System.out.println(longWithoutL);
 	}
 
 }
